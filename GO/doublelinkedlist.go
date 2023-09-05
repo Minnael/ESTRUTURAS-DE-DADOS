4f8b70a2eb3ed5da24ca3073a756a76141179ae5
Minnael <minnaelcampelo@gmail.com>
2023-09-05 11:40:45 -0300

    first implementation doublelinkedlist

--- GO/doublelinkedlist.go
+++ GO/doublelinkedlist.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+type List[T any] interface {
+	append(value T)
+}
+
+type Node[T any] struct {
+	value      int
+	prev, next *Node[T]
+}
+
+type DoubleLinkedList[T any] struct {
+	head, tail *Node[T]
+}
+
 func main() {
 	fmt.Println("Hello World")
 }
