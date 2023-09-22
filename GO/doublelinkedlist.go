f1f92822c6f2a02c32b3e1cd42de53059e53dfac
Minnael <minnaelcampelo@gmail.com>
2023-09-22 20:41:14 -0300

    update in DoubleLinkedList

--- GO/doublelinkedlist.go
+++ GO/doublelinkedlist.go
@@ -1,16 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type List[T any] interface {
-	append(val T)          //TERMINADO
-	show()                 //TERMINADO
-	pop()                  //TERMINADO
-	update(val T, pos int) //TERMINADO
-	remove(pos int)        //TERMINADO
-	insert(val T, pos int) //TERMINADO
-	get(pos int) T         //TERMINADO
-	reverse()              //TERMINADO
+	append(val T)           //TERMINADO
+	show()                  //TERMINADO
+	pop() (T, error)        //TERMINADO
+	update(val T, pos int)  //TERMINADO
+	get(pos int) (T, error) //TERMINADO
+	remove(pos int)         //TERMINADO
+	insert(val T, pos int)  //TERMINADO
+	reverse()               //TERMINADO
 }
 
 type Node[T any] struct {
@@ -20,137 +23,146 @@ type Node[T any] struct {
 
 type DoubleLinkedList[T any] struct {
 	head, tail *Node[T]
-	size       int
+	inserted   int
 }
 
 func (list *DoubleLinkedList[T]) append(val T) {
-	newNode := Node[T]{value: val, prev: nil, next: nil}
-	if list.size == 0 {
+	newNode := Node[T]{value: val, prev: list.tail, next: nil}
+	if list.inserted == 0 {
 		list.head = &newNode
 		list.tail = &newNode
-		list.size++
-	} else {
-		list.tail.next = &newNode
-		list.tail.next.prev = list.tail
-		list.tail = &newNode
-		list.size++
+		list.inserted++
+		return
 	}
+	list.tail.next = &newNode
+	list.tail = &newNode
+	list.inserted++
 }
 
 func (list *DoubleLinkedList[T]) show() {
 	aux := list.head
 	for aux != nil {
-		fmt.Print("|", aux.value, "|   ")
+		fmt.Print("|", aux.value, "|  ->  ")
 		aux = aux.next
 	}
+	fmt.Print("nil")
 }
 
-func (list *DoubleLinkedList[T]) pop() {
-	if list.size == 0 {
-		fmt.Println("EMPTY LIST")
-		return
+func (list *DoubleLinkedList[T]) pop() (T, error) {
+	if list.inserted == 0 {
+		var aux T
+		return aux, errors.New("EMPTY LIST")
 	}
-	if list.size == 1 {
+	if list.inserted == 1 {
+		sup := list.head.value
 		list.head = nil
 		list.tail = nil
-		return
+		list.inserted--
+		return sup, errors.New("")
 	}
-
-	aux := list.head
-
-	for aux.next.next != nil {
-		aux = aux.next
-	}
-	aux.next = nil
-	list.size--
+	sup := list.tail.value
+	list.tail.prev.next = nil
+	list.tail = list.tail.prev
+	list.inserted--
+	return sup, errors.New("")
 }
 
 func (list *DoubleLinkedList[T]) update(val T, pos int) {
-	if pos < 0 || pos > list.size-1 {
-		fmt.Println("INVALID POSITION")
+	if pos < 0 || pos > list.inserted-1 {
+		if pos == 0 {
+			fmt.Println("EMPTY LIST")
+		} else {
+			fmt.Println("INVALID POSITION")
+		}
 		return
 	}
-
-	if pos == 0 {
-		list.head.value = val
-		return
-	}
-
 	aux := list.head
-	for i := 0; i < pos-1; i++ {
+	for i := 0; i < pos; i++ {
 		aux = aux.next
 	}
-	aux.next.value = val
+	aux.value = val
+}
+
+func (list *DoubleLinkedList[T]) get(pos int) (T, error) {
+	if pos < 0 || pos > list.inserted-1 {
+		var aux T
+		if pos == 0 {
+			return aux, errors.New("EMPTY LIST")
+		} else {
+			return aux, errors.New("INVALID LIST")
+		}
+	}
+	aux := list.head
+	for i := 0; i < pos; i++ {
+		aux = aux.next
+	}
+	return aux.value, errors.New("")
 }
 
 func (list *DoubleLinkedList[T]) remove(pos int) {
-	if pos < 0 || pos > list.size-1 {
-		fmt.Println("INVALID POSITION")
+	if pos < 0 || pos > list.inserted-1 {
+		if pos == 0 {
+			fmt.Println("EMPTY LIST")
+		} else {
+			fmt.Println("INVALID POSITION")
+		}
 		return
 	}
-
 	if pos == 0 {
 		list.head = list.head.next
-		list.size--
+		list.inserted--
 		return
 	}
-
+	if pos == list.inserted-1 {
+		list.tail = list.tail.prev
+		list.tail.next = nil
+		list.inserted--
+		return
+	}
 	aux := list.head
-
-	for i := 0; i < pos-1; i++ {
+	for i := 1; i < pos; i++ {
 		aux = aux.next
 	}
 	aux.next = aux.next.next
-	list.size--
+	list.inserted--
 }
 
 func (list *DoubleLinkedList[T]) insert(val T, pos int) {
 	newNode := Node[T]{value: val, prev: nil, next: nil}
-	if pos < 0 || pos > list.size {
-		fmt.Println("INVALID POSITION")
+	if pos < 0 || pos > list.inserted {
+		if list.inserted == 0 {
+			list.head = &newNode
+			list.tail = &newNode
+			list.inserted++
+		} else {
+			fmt.Println("INVALID POSITION")
+		}
 		return
 	}
 	if pos == 0 {
-		sup := list.head
+		list.head.prev = &newNode
+		newNode.next = list.head
 		list.head = &newNode
-		newNode.next = sup
-		list.size++
-		return
+	} else if list.inserted == pos {
+		newNode.prev = list.tail
+		list.tail.next = &newNode
+		list.tail = &newNode
+	} else {
+		aux := list.head
+		for i := 1; i < pos; i++ {
+			aux = aux.next
+		}
+		newNode.next = aux.next
+		newNode.prev = aux
+		aux.next.prev = &newNode
+		aux.next = &newNode
 	}
-
-	aux := list.head
-
-	for i := 0; i < pos-1; i++ {
-		aux = aux.next
-	}
-	newNode.next = aux.next
-	aux.next = &newNode
-	list.size++
-}
-
-func (list *DoubleLinkedList[T]) get(pos int) T {
-	if pos < 0 || pos > list.size-1 {
-		fmt.Println("INVALID POSITION")
-		var sup T
-		return sup
-	}
-
-	if pos == 0 {
-		return list.head.value
-	}
-
-	aux := list.head
-
-	for i := 0; i < pos-1; i++ {
-		aux = aux.next
-	}
-	return aux.next.value
+	list.inserted++
 }
 
 func (list *DoubleLinkedList[T]) reverse() {
 	current := list.head
-	var prev *Node[T]
-	var next *Node[T]
+	var prev, next *Node[T]
 	for current != nil {
 		next = current.next
 		current.next = prev
@@ -161,14 +173,16 @@ func (list *DoubleLinkedList[T]) reverse() {
 }
 
 func main() {
-	list := DoubleLinkedList[int]{head: nil, tail: nil, size: 0}
+	list := DoubleLinkedList[int]{head: nil, tail: nil, inserted: 0}
 
-	list.append(5)
-	list.append(10)
-	list.append(15)
-	list.append(20)
-	list.append(25)
+	list.append(100)
+	list.append(200)
+	list.append(300)
+	list.append(400)
+	list.append(500)
 
+	list.insert(600, 5)
 	list.reverse()
+
 	list.show()
 }
