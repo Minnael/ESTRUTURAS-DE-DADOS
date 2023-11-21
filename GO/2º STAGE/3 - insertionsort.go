34efc87f2fd4b33ca29c191140410106ba3b9318
Minnael <minnaelcampelo@gmail.com>
2023-11-21 13:49:23 -0300

    small changes to the mergeSort

--- GO/2º STAGE/3 - insertionsort.go
+++ GO/2º STAGE/3 - insertionsort.go
@@ -2,20 +2,18 @@ package main
 
 import "fmt"
 
-func insertionSort(list []int) []int {
+func insertionsort(list []int) []int {
 	for i := 0; i < len(list)-1; i++ {
 		number := list[i+1]
 		for i >= 0 && list[i] > number {
-			list[i+1] = list[i]
+			list[i+1], list[i] = list[i], list[i+1]
 			i--
 		}
-		list[i+1] = number
 	}
 	return list
 }
 
 func main() {
-	list := []int{7, 5}
-
-	fmt.Println(insertionSort(list))
+	list := []int{3, 1, 8, 4, 1, 7, 5, 1, 5, 1, 8}
+	fmt.Println(insertionsort(list))
 }
---
+++ GO/2º STAGE/4 - mergesort.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+var soma int
+
+func mergeSort(lista []int) []int {
+	if len(lista) > 1 {
+		meio := len(lista) / 2
+		esquerda := mergeSort(lista[0:meio])
+		direita := mergeSort(lista[meio:len(lista)])
+		return merge(lista, esquerda, direita)
+	}
+	return lista
+}
+
+func merge(lista []int, esquerda []int, direita []int) []int {
+	i, j, lista_aux := 0, 0, make([]int, 0, len(esquerda)+len(direita))
+
+	for i < len(esquerda) && j < len(direita) {
+		if esquerda[i] < direita[j] {
+			lista_aux = append(lista_aux, esquerda[i])
+			i++
+		} else {
+			lista_aux = append(lista_aux, direita[j])
+			j++
+		}
+	}
+	lista_aux = append(lista_aux, esquerda[i:]...)
+	lista_aux = append(lista_aux, direita[j:]...)
+
+	for i := 0; i < len(lista_aux); i++ {
+		lista[i] = lista_aux[i]
+	}
+
+	return lista_aux
+}
+
+func main() {
+	lista := []int{3, 6, 2, 4, 8, 9, 7}
+	mergeSort(lista)
+	fmt.Println(lista)
+}
